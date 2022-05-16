e1156ceca2b88893ec09dd47edd6c1519b897e50
XHou4 <xiruihou@gmail.com>
2022-05-16 17:13:32 -0400

    Add files via upload

---
+++ routes/campgrounds.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const router = express.Router();
+const { campgroundSchema, reviewSchema } = require('../schemas')
+const catchAsync = require('../utils/catchAsync')
+const ExpressError = require('../utils/ExpressError')
+const Campground = require('../models/campground');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const campgrounds = require('../controllers/campgrounds');
+const { required } = require('joi');
+const multer = require('multer')
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showcampground))
+    .put(isLoggedIn, isAuthor,upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, catchAsync(campgrounds.deleteCampground))
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+
+module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console.error, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 200; i++) {
+        const random1000 = Math.floor((Math.random() * 1000));
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '60c8dbe59cb0b96fa7f12446',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: [{
+                url: 'https://source.unsplash.com/random/483251',
+                filename: 'random/483251'
+            }],
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis fuga tempora impedit? Optio sint harum dolore vitae aspernatur. Omnis itaque vel pariatur distinctio officiis, sequi cum error non deserunt commodi.',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            }
+        })
+
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
